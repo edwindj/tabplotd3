e51317451b8c7e68d89e7f5850e8d91fa1233875
Edwin de Jonge <edwindjonge@gmail.com>
2011-12-01 23:16:28 +0100

    minor tweaks

--- inst/app/numericvar.js
+++ inst/app/numericvar.js
@@ -2,19 +2,23 @@
 function numvar(data, container, offset, width, sc_bin, sc_complete){
 
 sc_x = d3.scale.linear()
-       .domain([d3.min(data.mean), d3.max(data.mean)])
+       .domain([0, d3.max(data.mean)])
        .range([0, width]);
 
 var vis = container.append("svg:g")
     .attr("class", "numvar")
-    .attr("transform", "translate(10)");
-    
+    .attr("transform", "translate("+offset+")");
+
+vis.append("svg:rect")
+   .attr("class", "panel")
+   .attr("width", width)
+   .attr("height", sc_bin(99))
+   .style("fill", "silver");
 var bars = vis.selectAll("g.bar")
     .data(data.mean)
   .enter().append("svg:g")
     .attr("class", "bar")
     .attr("transform", function(d, i) { return "translate(0," + sc_bin(i) + ")"; });
-
 bars.append("svg:rect")
     .attr("fill", function(d,i){return sc_complete(data.compl[i])})
     .attr("width", sc_x)
