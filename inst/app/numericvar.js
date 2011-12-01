d53f7e53039d1e8ccc5cb0e14aa88251fd9aae15
Edwin de Jonge <edwindjonge@gmail.com>
2011-12-01 16:54:22 +0100

    further improvements

---
+++ inst/app/numericvar.js
@@ -0,0 +1,32 @@
+
+function numvar(data, container, offset, width, sc_bin, sc_complete){
+
+sc_x = d3.scale.linear()
+       .domain([d3.min(data.mean), d3.max(data.mean)])
+       .range([0, width]);
+
+var vis = container.append("svg:g")
+    .attr("class", "numvar")
+    .attr("transform", "translate(10)");
+    
+var bars = vis.selectAll("g.bar")
+    .data(data.mean)
+  .enter().append("svg:g")
+    .attr("class", "bar")
+    .attr("transform", function(d, i) { return "translate(0," + sc_bin(i) + ")"; });
+
+bars.append("svg:rect")
+    .attr("fill", function(d,i){return sc_complete(data.compl[i])})
+    .attr("width", sc_x)
+    .attr("height", sc_bin.rangeBand());
+
+bars.append("svg:line")
+    .style("stroke-width", 1)
+    .style("stroke", "steelblue")
+    .attr("x1", sc_x)
+    .attr("x2", sc_x)
+    .attr("y1", 0)
+    .attr("y2", sc_bin.rangeBand());
+    
+    return vis;
+}
