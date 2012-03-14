e0118d89dffcea1beb0496cc6fb9f3b678ccef44
Edwin de Jonge <edwindjonge@gmail.com>
2012-03-14 14:44:16 +0100

    tweaking

--- R/itabplot.R
+++ R/itabplot.R
@@ -1,10 +1,15 @@
-.app <- NULL
+s <- NULL
 
 #' itabplot
-#' @param data data
 #' @export
-itabplot <- function(data){
-  if (is.null(.app)){
-    .app <- RhttpdApp
+itabplot <- function(...){
+  if (is.null(s)){
+    s <- Rhttpd$new()
+    s$launch( name='tabplotd3'
+            , app=system.file('app/config.R', package='tabplotd3')
+            , ...
+            )
   }
+  
+  
 }
