ca0a1733956ed49a801f84065b9aebd5752c4996
Edwin de Jonge <edwindjonge@gmail.com>
2012-04-13 08:49:30 +0200

    fixed configuration part

--- R/itabplot.R
+++ R/itabplot.R
@@ -2,14 +2,18 @@ s <- NULL
 
 #' itabplot
 #' @export
-itabplot <- function(...){
+itabplot <- function(x, ...){
+  tp <- tableplot(x, plot=FALSE)
+  #toJSON(tp)
   if (is.null(s)){
     s <- Rhttpd$new()
     s$launch( name='tabplotd3'
             , app=system.file('app/config.R', package='tabplotd3')
             , ...
             )
-  }
-  
-  
+  } 
+}
+
+render_json <- function(x){
+   RJSONIO::toJSON(x)
 }
--- inst/app/config.R
+++ inst/app/config.R
@@ -1,8 +1,14 @@
 app <- Builder$new( Static$new( urls = c('/css','/img','/js')
-                              , root = '.'
+                              , root = system.file("app", package="tabplotd3")
                               )
-                  , URLMap$new( '/json' = function(env){
+                  , URLMap$new( '^/json' = function(env){
                                 }
-                              , '.' = Redirect$new('index.html')                        
+                              , ".*" = function(env){
+                                   req <- Request$new(env)
+                                   res <- Response$new()
+                                   res$redirect("/index.html")
+                                }
                               )
                   )
+
+s$launch(app, "app")
