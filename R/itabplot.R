ac1a5f5ec72dd4c2aa986eb30a8aea2a3bbe07c1
Edwin de Jonge <edwindjonge@gmail.com>
2012-05-04 11:45:14 +0200

    system.file

--- R/itabplot.R
+++ R/itabplot.R
@@ -1,5 +1,15 @@
 s <- NULL
 
+
+system.file <- function(..., pkg=NULL, package = "base", lib.loc = NULL, mustWork = FALSE){
+  pkg <- as.package(pkg)
+  if (package == pkg$package){
+    file.path(normalizePath(pkg$path, winslash="/"), "inst", ...)
+  } else {
+    base::system.file(..., package=package, lib.loc=lib.loc, mustWork=mustWork)
+  }
+}
+
 #' itabplot
 #' @export
 itabplot <- function(x, ...){
